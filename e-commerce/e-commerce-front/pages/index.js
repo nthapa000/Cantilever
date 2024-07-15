0f4ada7f1ea8225d410263b83930f432aa63826b
nthapa000 <nishantthapa0000@gmail.com>
2024-07-15 23:26:39 +0530

    News Website Docs + Ecommerce front , Latest Product

--- e-commerce/e-commerce-front/pages/index.js
+++ e-commerce/e-commerce-front/pages/index.js
@@ -4,13 +4,13 @@ import NewProducts from "@/components/NewProduct";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/model/Product";
 
-export default function Homepage({product}) {
+export default function Homepage({featuredProduct,newProducts}) {
   // console.log(product);
   return (
     <div className="roboto-regular">
       <Header />
-      <Featured  product={product}/>
-      <NewProducts/>
+      <Featured  product={featuredProduct}/>
+      <NewProducts products={newProducts}/>
     </div>
   );
 }
@@ -18,8 +18,13 @@ export default function Homepage({product}) {
 export async function getServerSideProps() {
   const FeaturedProductId = "668e4d317cc6e41ed18d8c4a";
   await mongooseConnect();
-  const product = await Product.findById(FeaturedProductId);
+  const featuredProduct = await Product.findById(FeaturedProductId);
+  // -1 descending, last updated product is basically the latest product
+  const newProducts = await Product.find({},null,{sort:{'_id':-1},limit:10});
   return { 
-    props: { product:JSON.parse(JSON.stringify(product)) } 
+    props: {
+      featuredProduct:JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts:JSON.parse(JSON.stringify(newProducts))
+    } 
   };
 }
